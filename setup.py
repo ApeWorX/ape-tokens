dff241a96eead4b92592564d56bbb5772b653b0d
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2021-07-13 19:01:58 -0400

    refactor: upgrade to latest project template

--- setup.py
+++ setup.py
@@ -1,39 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages  # type: ignore
-
+from setuptools import find_packages, setup  # type: ignore
 
 extras_require = {
-    "test": [
-        "pytest>=6.0.1,<7",
-        "pytest-xdist",
-        "pytest-coverage",
+    "test": [  # `test` GitHub Action jobs uses this
+        "pytest>=6.0,<7.0",  # Core testing package
+        "pytest-xdist",  # multi-process runner
+        "pytest-cov",  # Coverage analyzer plugin
+        "hypothesis>=6.2.0,<7.0",  # Strategy-based fuzzer
     ],
     "lint": [
-        "black==19.10b0",
-        "flake8==3.8.4",
-        "isort>=5.7.0,<6",
-        "mypy==0.790",
-        "pydocstyle>=5.1.1,<6",
+        "black>=20.8b1,<21.0",  # auto-formatter and linter
+        "mypy>=0.800,<1.0",  # Static type analyzer
+        "flake8>=3.8.3,<4.0",  # Style linter
+        "isort>=5.7.0,<6.0",  # Import sorting linter
     ],
     "doc": [
-        "Sphinx>=3.4.3,<4",
-        "sphinx_rtd_theme>=0.5.1",
+        "Sphinx>=3.4.3,<4",  # Documentation generator
+        "sphinx_rtd_theme>=0.1.9,<1",  # Readthedocs.org theme
+        "towncrier>=19.2.0, <20",  # Generate release notes
+    ],
+    "release": [  # `release` GitHub Action job uses this
+        "setuptools",  # Installation tool
+        "setuptools-scm",  # Installation tool
+        "wheel",  # Packaging tool
+        "twine",  # Package upload tool
     ],
     "dev": [
-        "pytest-watch>=4.2.0,<5",
-        "wheel",
-        "twine",
-        "ipython",
+        "commitizen",  # Manage commits and publishing releases
+        "pre-commit",  # Ensure that linters are run prior to commiting
+        "pytest-watch",  # `ptw` test watcher/runner
+        "IPython",  # Console for interacting
+        "ipdb",  # Debugger (Must use `export PYTHONBREAKPOINT=ipdb.set_trace`)
     ],
 }
 
 extras_require["dev"] = (
-    extras_require["dev"]
-    + extras_require["test"]  # noqa: W504
-    + extras_require["lint"]  # noqa: W504
-    + extras_require["doc"]  # noqa: W504
+    extras_require["test"]
+    + extras_require["lint"]
+    + extras_require["doc"]
+    + extras_require["release"]
+    + extras_require["dev"]
 )
 
 with open("./README.md") as readme:
@@ -41,29 +49,39 @@ with open("./README.md") as readme:
 
 setup(
     name="ape-tokens",
-    version="0.0.0a1",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description="ape-tokens: tokenlists plugin for Ape",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="ApeWorX Ltd.",
     author_email="admin@apeworx.io",
-    url="https://github.com/apeworx/ape-tokens",
+    url="https://github.com/ApeWorX/ape-tokens",
     include_package_data=True,
     python_requires=">=3.6, <4",
-    install_requires=["eth-brownie>=1.13.0,<2", "tokenlists<1.0"],
+    install_requires=[
+        "importlib-metadata ; python_version<'3.8'",
+        "eth-ape>=0.1.0a14,<0.2",
+        "tokenlists",
+    ],
     extras_require=extras_require,
     py_modules=["ape_tokens"],
-    license="Apache License 2.0",
+    license="Apache-2.0",
     zip_safe=False,
     keywords="ethereum",
     packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={"ape_tokens": ["py.typed"]},
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
