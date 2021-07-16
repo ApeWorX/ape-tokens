f1c3a2f5b1e5e72b22303d711bce23bba8c5fd73
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2021-07-15 22:35:19 -0400

    refactor: updated API

--- ape_tokens/__init__.py
+++ ape_tokens/__init__.py
@@ -1,10 +1,8 @@
 from ape import plugins
-from ape.managers.converters import SIGNED_INTEGERS, UNSIGNED_INTEGERS
 
 from .converters import TokenConversions
 
 
 @plugins.register(plugins.ConversionPlugin)
 def converters():
-    for abi_type in SIGNED_INTEGERS + UNSIGNED_INTEGERS:
-        yield abi_type, TokenConversions
+    yield int, TokenConversions
--- ape_tokens/converters.py
+++ ape_tokens/converters.py
@@ -1,33 +1,43 @@
 from decimal import Decimal
+from typing import Any, Optional
 
 from ape.api import ConverterAPI
-from tokenlists import TokenInfo, TokenListManager  # type: ignore
-
-manager = TokenListManager()
-
-
-def get_token(symbol: str, chain_id: int = None) -> TokenInfo:
-    breakpoint()
-    return manager.get_token_info(symbol, chain_id=chain_id)
+from tokenlists import TokenListManager
 
 
 class TokenConversions(ConverterAPI):
     """Converts token amounts like `100 LINK` to 1e18"""
 
-    def is_convertible(self, value: str) -> bool:
-        try:
-            return self.convert(value) is not None
+    manager: Optional[TokenListManager] = None
 
-        except Exception:
+    def __post_init__(self):
+        self.manager = TokenListManager()
+
+    def is_convertible(self, value: Any) -> bool:
+        if not isinstance(value, str):
             return False
 
-    def convert(self, value: str) -> int:
-        value, symbol = value.split(" ")
+        if " " not in value or len(value.split(" ")) > 2:
+            return False
+
+        _, symbol = value.split(" ")
 
         provider = self.networks.active_provider
         if not provider:
             raise Exception("Not connected to a provider!")
 
-        token = get_token(symbol, chain_id=provider.network.chain_id)
+        assert self.manager  # Really just to help mypy
+        tokens = self.manager.get_tokens(chain_id=provider.network.chain_id)
+
+        return symbol in map(lambda t: t.symbol, tokens)
+
+    def convert(self, value: str) -> int:
+        value, symbol = value.split(" ")
+
+        provider = self.networks.active_provider
+        assert provider  # Really just to help mypy
+
+        assert self.manager  # Really just to help mypy
+        token = self.manager.get_token_info(symbol, chain_id=provider.network.chain_id)
 
         return int(Decimal(value) * 10 ** token.decimals)
