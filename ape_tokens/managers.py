f759fb7d99ad08dac29ba64b5cb8413d96a5118e
El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>
2022-02-04 11:48:10 -0500

    fix: add missing ERC20 ABI Methods (#9)

--- ape_tokens/managers.py
+++ ape_tokens/managers.py
@@ -39,6 +39,67 @@ ERC20 = ContractType(
                 "inputs": [{"name": "holder", "type": "address"}],
                 "outputs": [{"type": "uint256"}],
             },
+            {
+                "type": "function",
+                "stateMutability": "nonpayable",
+                "name": "transfer",
+                "inputs": [
+                    {"name": "receiver", "type": "address"},
+                    {"name": "amount", "type": "uint256"},
+                ],
+                "outputs": [{"type": "bool"}],
+            },
+            {
+                "type": "function",
+                "stateMutability": "nonpayable",
+                "name": "approve",
+                "inputs": [
+                    {"name": "spender", "type": "address"},
+                    {"name": "amount", "type": "uint256"},
+                ],
+                "outputs": [{"type": "bool"}],
+            },
+            {
+                "type": "function",
+                "stateMutability": "view",
+                "name": "allowance",
+                "inputs": [
+                    {"name": "owner", "type": "address"},
+                    {"name": "spender", "type": "address"},
+                ],
+                "outputs": [{"type": "uint256"}],
+            },
+            {
+                "type": "function",
+                "stateMutability": "nonpayable",
+                "name": "transferFrom",
+                "inputs": [
+                    {"name": "sender", "type": "address"},
+                    {"name": "receiver", "type": "address"},
+                    {"name": "amount", "type": "uint256"},
+                ],
+                "outputs": [{"type": "bool"}],
+            },
+            {
+                "type": "event",
+                "name": "Transfer",
+                "anonymous": False,
+                "inputs": [
+                    {"name": "sender", "indexed": True, "type": "address"},
+                    {"name": "receiver", "indexed": True, "type": "address"},
+                    {"name": "amount", "indexed": False, "type": "uint256"},
+                ],
+            },
+            {
+                "type": "event",
+                "name": "Approval",
+                "anonymous": False,
+                "inputs": [
+                    {"name": "owner", "indexed": True, "type": "address"},
+                    {"name": "spender", "indexed": True, "type": "address"},
+                    {"name": "amount", "indexed": False, "type": "uint256"},
+                ],
+            },
         ],
     }
 )
