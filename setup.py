0d7b589827a7d2a1740c2a7f7bc328460dd33888
unparalleled-js <yingthi@live.com>
2022-08-22 12:32:23 -0500

    chore: rm outdated stuff from setup.cfg

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     python_requires=">=3.7.2,<4",
     install_requires=[
         "importlib-metadata ; python_version<'3.8'",
-        "eth-ape>=0.4.4,<0.5.0",
+        "eth-ape>=0.4.4,<0.5",
         "tokenlists>=0.1.2",
     ],
     entry_points={
