ec5583bf175dc7173cc45db8794595e77d7996ab
ibis <toni@apeworx.io>
2022-11-07 15:51:25 -0600

    fix: pin mypy to 0.982

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ extras_require = {
     ],
     "lint": [
         "black>=22.6.0,<23.0",  # auto-formatter and linter
-        "mypy>=0.971,<1.0",  # Static type analyzer
+        "mypy==0.982",  # Static type analyzer
         "flake8>=4.0.1,<5.0",  # Style linter
         "isort>=5.10.1,<6.0",  # Import sorting linter
     ],
