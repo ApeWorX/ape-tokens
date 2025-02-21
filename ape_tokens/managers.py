3bc8726c1052e0512d17bf9fb02440b00c709c0d
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2025-02-21 16:28:58 -0500

    feat: configure TokenListManager automatically using custom ape config

--- ape_tokens/managers.py
+++ ape_tokens/managers.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING
 
 from ape.exceptions import ContractNotFoundError
+from ape.logging import get_logger
 from ape.utils import ManagerAccessMixin, cached_property
 from eth_utils import to_checksum_address
 from tokenlists import TokenListManager
@@ -10,12 +11,37 @@ from .types import ERC20
 if TYPE_CHECKING:
     from ape.contracts import ContractInstance
 
+    from .config import TokensConfig
+
+logger = get_logger(__package__)
 
 
 class TokenManager(ManagerAccessMixin, dict):
+    @property
+    def config(self) -> "TokensConfig":
+        return self.config_manager.get_config("tokens")
+
     @cached_property
     def _manager(self) -> TokenListManager:
-        return TokenListManager()
+        manager = TokenListManager()
+
+        for required_tokenlist in self.config.required:
+            if required_tokenlist.name not in manager.installed_tokenlists:
+                if (
+                    installed_name := manager.install_tokenlist(required_tokenlist.uri)
+                ) != required_tokenlist.name:
+                    # TODO: Allow setting custom name via `TokenListManager.install_tokenlist` to avoid this
+                    logger.warning(
+                        f"Installed list name '{installed_name}' does not match "
+                        f"requirement '{required_tokenlist.name}'. This could be problematic."
+                    )
+
+        # TODO: Allow sourcing from multiple lists in `TokenListManager`,
+        #       and set priority here instead
+        if default_selected := self.config.default:
+            manager.set_default_tokenlist(default_selected)
+
+        return manager
 
     def __repr__(self) -> str:
         return f"<ape_tokens.TokenManager default='{self._manager.default_tokenlist}'>"
