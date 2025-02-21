3945142dafc33876a3606b4628d0c3a3982635b9
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2025-02-21 16:28:01 -0500

    refactor: move ERC20 .types just to make space

--- ape_tokens/managers.py
+++ ape_tokens/managers.py
@@ -1,113 +1,15 @@
 from typing import TYPE_CHECKING
 
 from ape.exceptions import ContractNotFoundError
-from ape.types import ContractType
 from ape.utils import ManagerAccessMixin, cached_property
 from eth_utils import to_checksum_address
 from tokenlists import TokenListManager
 
+from .types import ERC20
+
 if TYPE_CHECKING:
     from ape.contracts import ContractInstance
 
-ERC20 = ContractType.model_validate(
-    {
-        "contractName": "ERC20",
-        "abi": [
-            {
-                "type": "function",
-                "stateMutability": "view",
-                "name": "name",
-                "outputs": [{"type": "string"}],
-            },
-            {
-                "type": "function",
-                "stateMutability": "view",
-                "name": "symbol",
-                "outputs": [{"type": "string"}],
-            },
-            {
-                "type": "function",
-                "stateMutability": "view",
-                "name": "decimals",
-                "outputs": [{"type": "uint8"}],
-            },
-            {
-                "type": "function",
-                "stateMutability": "view",
-                "name": "totalSupply",
-                "outputs": [{"type": "uint256"}],
-            },
-            {
-                "type": "function",
-                "stateMutability": "view",
-                "name": "balanceOf",
-                "inputs": [{"name": "holder", "type": "address"}],
-                "outputs": [{"type": "uint256"}],
-            },
-            {
-                "type": "function",
-                "stateMutability": "nonpayable",
-                "name": "transfer",
-                "inputs": [
-                    {"name": "receiver", "type": "address"},
-                    {"name": "amount", "type": "uint256"},
-                ],
-                "outputs": [{"type": "bool"}],
-            },
-            {
-                "type": "function",
-                "stateMutability": "nonpayable",
-                "name": "approve",
-                "inputs": [
-                    {"name": "spender", "type": "address"},
-                    {"name": "amount", "type": "uint256"},
-                ],
-                "outputs": [{"type": "bool"}],
-            },
-            {
-                "type": "function",
-                "stateMutability": "view",
-                "name": "allowance",
-                "inputs": [
-                    {"name": "owner", "type": "address"},
-                    {"name": "spender", "type": "address"},
-                ],
-                "outputs": [{"type": "uint256"}],
-            },
-            {
-                "type": "function",
-                "stateMutability": "nonpayable",
-                "name": "transferFrom",
-                "inputs": [
-                    {"name": "sender", "type": "address"},
-                    {"name": "receiver", "type": "address"},
-                    {"name": "amount", "type": "uint256"},
-                ],
-                "outputs": [{"type": "bool"}],
-            },
-            {
-                "type": "event",
-                "name": "Transfer",
-                "anonymous": False,
-                "inputs": [
-                    {"name": "sender", "indexed": True, "type": "address"},
-                    {"name": "receiver", "indexed": True, "type": "address"},
-                    {"name": "amount", "indexed": False, "type": "uint256"},
-                ],
-            },
-            {
-                "type": "event",
-                "name": "Approval",
-                "anonymous": False,
-                "inputs": [
-                    {"name": "owner", "indexed": True, "type": "address"},
-                    {"name": "spender", "indexed": True, "type": "address"},
-                    {"name": "amount", "indexed": False, "type": "uint256"},
-                ],
-            },
-        ],
-    }
-)
 
 
 class TokenManager(ManagerAccessMixin, dict):
